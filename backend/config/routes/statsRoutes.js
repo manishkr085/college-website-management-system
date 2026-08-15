9aabff9bb43d9ce33017d26d749fbeb39ba89dac
manishkr085 <yes.manishkr@gmail.com>
2026-08-15 14:59:04 +0530

    Delete backend directory

--- backend/config/routes/statsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDashboardStats, triggerSeedData } = require('../controllers/statsController');
-
-router.get('/dashboard', getDashboardStats);
-router.post('/seed', triggerSeedData);
-
-module.exports = router;
